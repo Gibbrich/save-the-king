25d19e65246567c66f63d726aedccabf518950a3
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2020-10-01 18:25:27 +0300

    Add ability to display spawn points

---
+++ Assets/Game/Scripts/LevelManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public int soldiersCount;
+    }
+}
--- Assets/Game/Scripts/PlayerInputController.cs
+++ Assets/Game/Scripts/PlayerInputController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Gamelogic.Extensions;
 using InControl;
 using UnityEngine;
 
@@ -9,21 +10,30 @@ namespace Game.Scripts
     [RequireComponent(typeof(LineRenderer))]
     public class PlayerInputController : MonoBehaviour
     {
+        public GameObject spawnPointPrefab;
         public float minPointDistance;
         public float lineRenderHeight = 0.5f;
+        public float spawnPointDistance = 0.5f;
+        public float spawnPointRadius = 0.1f;
 
         private LineRenderer lineRenderer;
         private List<Vector3> points;
+        private List<int> spawnPointsIndices;
         private float minDistanceSquare;
+        private float spawnPointDistanceSquare;
         private Camera camera;
-        private RaycastHit[] results = new RaycastHit[5];
+        private RaycastHit[] results = new RaycastHit[1];
+        private Pool<GameObject> spawnPointPool;
 
         private void Start()
         {
             lineRenderer = GetComponent<LineRenderer>();
             points = new List<Vector3>();
+            spawnPointsIndices = new List<int>();
             minDistanceSquare = Mathf.Pow(minPointDistance, 2);
+            spawnPointDistanceSquare = Mathf.Pow(spawnPointDistance, 2);
             camera = Camera.main;
+            spawnPointPool = new Pool<GameObject>(50, CreateSpawnPoint, Destroy, WakeUpSpawnPoint, SetToSleepSpawnPoint);
         }
 
         private void Update()
@@ -35,6 +45,7 @@ namespace Game.Scripts
                 if (touch.phase == TouchPhase.Began)
                 {
                     points.Clear();
+                    spawnPointPool.SetAllItemsToSleep();
                 }
                 else
                 {
@@ -49,31 +60,60 @@ namespace Game.Scripts
             var hitsCount = Physics.RaycastNonAlloc(ray, results, 30f);
             if (hitsCount >= 1)
             {
-                var worldPoint = results[0].point;
-                worldPoint.y = lineRenderHeight;
+                var raycastHit = results[0];
+
+                if (results[0].transform.CompareTag("Floor"))
+                {
+                    var worldPoint = raycastHit.point;
+                    worldPoint.y = lineRenderHeight;
                 
-                if (points.Count == 0)
-                {
-                    AddPoint(worldPoint);
-                }
-                else
-                {
-                    var lastPoint = points[points.Count - 1];
-                    var sqrDistance = (worldPoint - lastPoint).sqrMagnitude;
-                    if (sqrDistance >= minDistanceSquare)
+                    if (points.Count == 0)
                     {
-                        AddPoint(worldPoint);
+                        AddPoint(worldPoint, true);
+                    }
+                    else
+                    {
+                        var lastPoint = points[points.Count - 1];
+                        var sqrDistance = (worldPoint - lastPoint).sqrMagnitude;
+                        if (sqrDistance >= minDistanceSquare)
+                        {
+                            var previousSpawnPointIndex = spawnPointsIndices[spawnPointsIndices.Count - 1];
+                            var shouldAddSpawnPoint = (worldPoint - points[previousSpawnPointIndex]).sqrMagnitude >= spawnPointDistanceSquare;
+                            AddPoint(worldPoint, shouldAddSpawnPoint);
+                        }
                     }
                 }
             }
         }
 
-        private void AddPoint(Vector3 worldPoint)
+        private void AddPoint(Vector3 worldPoint, bool shouldAddSpawnPoint)
         {
             points.Add(worldPoint);
             var pointsCount = points.Count;
             lineRenderer.positionCount = pointsCount;
             lineRenderer.SetPosition(pointsCount - 1, worldPoint);
+            
+            if (shouldAddSpawnPoint)
+            {
+                spawnPointsIndices.Add(points.Count - 1);
+                spawnPointPool.GetNewObject();
+            }
         }
+
+        private GameObject CreateSpawnPoint()
+        {
+            var spawnPoint = Instantiate(spawnPointPrefab, transform);
+            spawnPoint.transform.localScale = new Vector3(spawnPointRadius, spawnPointRadius, spawnPointRadius);
+            return spawnPoint;
+        }
+
+        private void WakeUpSpawnPoint(GameObject spawnPoint)
+        {
+            var spawnPointsIndex = spawnPointsIndices[spawnPointsIndices.Count - 1];
+            spawnPoint.transform.position = points[spawnPointsIndex];
+            spawnPoint.SetActive(true);
+        }
+
+        private void SetToSleepSpawnPoint(GameObject spawnPoint) => spawnPoint.SetActive(false);
     }
 }
